06f941692b5573fc38b9c6ca1ab00eab327f4c05
Louis Huang <0563louishuang@gmail.com>
2017-03-10 18:23:11 -0800

    [feature] increment by click

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-/**
- * Created by louishuang on 3/10/17.
- */
+'use strict';
+
+(function () {
+    document.addEventListener('DOMContentLoaded', function() {
+      var circleList = document.getElementsByClassName('circle');
+
+      function setupClickHandler(element){
+        var count = 0;
+        element.addEventListener('click', function() {
+          count++;
+          this.lastElementChild.firstElementChild.innerText = count;
+          console.log(count);
+        });
+      }
+      for (var i =0; i < circleList.length; i++) {
+        var element = circleList[i];
+        setupClickHandler(element);
+        console.log(this.lastElementChild.innerText);
+      }
+    });
+  }
+)();
