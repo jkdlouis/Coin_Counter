108ce2d00be78ee0cf91971db9f0fead8a5e7a6e
Louis Huang <0563louishuang@gmail.com>
2017-03-11 17:41:20 -0800

    [feature] total handler + notification

--- main.js
+++ main.js
@@ -4,18 +4,34 @@
     document.addEventListener('DOMContentLoaded', function() {
       var circleList = document.getElementsByClassName('circle');
 
-      function setupClickHandler(element){
+      function setupClickHandler(element) {
         var count = 0;
         element.addEventListener('click', function() {
           count++;
           this.lastElementChild.firstElementChild.innerText = count;
-          console.log(count);
         });
       }
+      function setupTotalHandler(element) {
+        element.addEventListener('click', function() {
+          total += Number(element.firstElementChild.value);
+          document.getElementById('total').value = total;
+        });
+      }
+      function setupNotification(element) {
+        document.getElementById('calculate').addEventListener('click', function() {
+          element.lastElementChild.classList.remove('hidden');
+          console.log(element.lastElementChild.classList);
+        });
+      }
+      //document.getElementById('total').addEventListener('click', function() {
+      //  console.log('works');
+      //});
       for (var i =0; i < circleList.length; i++) {
+        var total = 0;
         var element = circleList[i];
         setupClickHandler(element);
-        console.log(this.lastElementChild.innerText);
+        setupTotalHandler(element);
+        setupNotification(element);
       }
     });
   }
