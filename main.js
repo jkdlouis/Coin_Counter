0e85ed972f26f41980a16e0f34ef7c5aa0dca17a
Louis Huang <0563louishuang@gmail.com>
2017-03-12 22:05:23 -0700

    [feature] remove count handler, add notification value handler

--- main.js
+++ main.js
@@ -4,33 +4,42 @@
     document.addEventListener('DOMContentLoaded', function() {
       var circleList = document.getElementsByClassName('circle');
 
-      function setupClickHandler(element) {
-        var count = 0;
-        element.addEventListener('click', function() {
-          count++;
-          this.lastElementChild.firstElementChild.innerText = count;
-        });
-      }
       function setupTotalHandler(element) {
         element.addEventListener('click', function() {
           total += Number(element.firstElementChild.value);
           document.getElementById('total').value = total;
         });
       }
-      function setupNotification(element) {
+      function setupNotificationValue(element) {
         document.getElementById('calculate').addEventListener('click', function() {
-          element.lastElementChild.classList.remove('hidden');
-          console.log(element.lastElementChild.classList);
+          var number = Number(element.firstElementChild.value);
+          var sum = Number(document.getElementById('total').value);
+          var remainder;
+
+          if(sum === number && sum % number == 0) {
+            element.lastElementChild.firstElementChild.innerText = '1';
+          }
+
+          while(remainder !== number) {
+            remainder = sum % number;
+            element.lastElementChild.firstElementChild.innerText = remainder;
+            break;
+          }
         });
       }
-      //document.getElementById('total').addEventListener('click', function() {
-      //  console.log('works');
-      //});
+      function setupNotification(element) {
+        document.getElementById('calculate').addEventListener('click', function() {
+          //setupNotificationValue(element.firstElementChild.value);
+          if(element.lastElementChild.firstElementChild.innerText) {
+              element.lastElementChild.classList.remove('hidden');
+          }
+        });
+      }
       for (var i =0; i < circleList.length; i++) {
         var total = 0;
         var element = circleList[i];
-        setupClickHandler(element);
         setupTotalHandler(element);
+        setupNotificationValue(element);
         setupNotification(element);
       }
     });
