177051a212b0eed37649879c9d3b93467d642d08
Louis Huang <0563louishuang@gmail.com>
2017-03-15 15:04:14 -0700

    sortCoinArray + validation changed

--- main.js
+++ main.js
@@ -13,27 +13,32 @@
           coinThree = document.getElementById('coinThree'),
           coinFour  = document.getElementById('coinFour');
 
-      function setupCoinArray(element) {
-        coinArray.push(Number(element.firstElementChild.value));
+      function sortCoinArray() {
         coinArray.sort(function(a, b) {
           return b - a;
         });
+      }
+
+      function setupCoinArray(element) {
+        coinArray.push(Number(element.firstElementChild.value));
+        sortCoinArray();
       }
 
       function coinArrayHandler(element) {
         setupCoinArray(element);
         element.firstElementChild.addEventListener('blur', function() {
           var number = Number(element.firstElementChild.value);
-          if(number >= coinArray[0] || number < coinArray[0]) {
+
+          if(number >= coinArray[0]) {
             coinArray.splice(0, 1, number);
 
-          }else if(number >= coinArray[1] || number < coinArray[1]) {
+          }else if(number >= coinArray[1]) {
             coinArray.splice(1, 1, number);
 
-          }else if(number >= coinArray[2] || number < coinArray[2]) {
+          }else if(number >= coinArray[2]) {
             coinArray.splice(2, 1, number);
-
           }
+          sortCoinArray();
         });
       }
 
@@ -67,7 +72,7 @@
           coinThree.nextElementSibling.firstElementChild.innerText = count;
         }
         if(coinArray[3] > 0) {
-          count = total;
+          count = remainder;
           coinFour.nextElementSibling.firstElementChild.innerText = count;
         }
       });
